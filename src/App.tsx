495446b7118eb229f16cc2ca115486490a2cf860
rodleyorosa <rodleyorosa@gmail.com>
2026-01-01 21:05:46 +0100

    feat: add random quote fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { useFetchQuote } from "./hooks/useFetchQuote";
+import type { Quote } from "./types";
 
-function App() {
+const App = () => {
+  const { data, isLoading } = useFetchQuote<Quote>(
+    "https://thequoteshub.com/api/random"
+  );
   const [inputValue, setInputValue] = useState("");
   const [timeLeft, setTimeLeft] = useState(60);
   const [isStarted, setIsStarted] = useState(false);
-  // todo: replace with random string generator
-  const stringToCopy = "React is a JavaScript library for building interfaces.";
-  const stringToCopyArray = useMemo(
-    () => stringToCopy.split(""),
-    [stringToCopy]
-  );
+
+  const quote = useMemo(() => {
+    return (
+      data?.text ?? "React is a JavaScript library for building interfaces."
+    );
+  }, [data?.text]);
 
   const stringColorMapping = useMemo(() => {
-    return stringToCopyArray.map((char, index) => {
+    return quote.split("").map((char, index) => {
       let charColor = "";
 
       if (inputValue && inputValue[index]) {
@@ -26,7 +31,7 @@ function App() {
         </span>
       );
     });
-  }, [stringToCopyArray, inputValue]);
+  }, [quote, inputValue]);
 
   const handleOnChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -54,7 +59,7 @@ function App() {
 
   return (
     <div className="flex h-screen bg-gray-50">
-      <div className="flex flex-col m-auto gap-4 min-w-1/3">
+      <div className="flex flex-col m-auto gap-4 w-1/2">
         <div className="text-center">
           <h1 className="text-4xl font-bold">TypeFast</h1>
           <p className="text-gray-400 text-sm mt-2 font-light tracking-wide">
@@ -76,8 +81,12 @@ function App() {
           </div>
         </div>
 
-        <div className="bg-white p-6 shadow-sm rounded-md">
-          <p className="text-xl text-gray-400">{stringColorMapping}</p>
+        <div className="bg-white p-6 shadow-sm rounded-md min-h-32 flex items-center justify-center">
+          {isLoading ? (
+            <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-blue-600 border-r-transparent"></div>
+          ) : (
+            <p className="text-xl text-gray-400">{stringColorMapping}</p>
+          )}
         </div>
 
         <div className="bg-white p-6 shadow-sm rounded-md">
@@ -89,6 +98,20 @@ function App() {
           ></textarea>
         </div>
 
+        {timeLeft === 0 && (
+          <div className="flex flex-col gap-2 bg-green-100 border border-green-200 p-6 shadow-sm rounded-md text-center">
+            <h2 className="font-bold text-xl text-green-800">
+              Test Completed!
+            </h2>
+            <p>
+              <span className="font-bold">1</span> WPM
+            </p>
+            <p>
+              <span className="font-bold">100%</span> Accuracy
+            </p>
+          </div>
+        )}
+
         <button
           onClick={resetTime}
           className="bg-blue-600 text-white p-3 rounded-md font-semibold cursor-pointer"
@@ -98,6 +121,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useFetchQuote.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useFetchQuote = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+        setHasError(true);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, hasError };
+};
